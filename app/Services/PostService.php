0128cba46e569cfded91cfcae517c1abdaa18e0a
Jora Shahbazyan <jokoshahbazyann@gmail.com>
2017-12-29 15:38:31 +0400

    refactroing codes

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -8,12 +8,12 @@ use File;
 
 class PostService
 {
-  	protected $posts;
+    protected $posts;
 
-  	function __construct(Post $posts)
-  	{
-    	$this->posts = $posts;
-  	}
+    function __construct(Post $posts)
+    {
+        $this->posts = $posts;
+    }
 
     public function getAllPosts()
     {
@@ -22,53 +22,53 @@ class PostService
                     ->get();
     }
 
-   	public function getPostsByUserId($id)
-  	{
-    	return $this->posts
+    public function getPostsByUserId($id)
+    {
+        return $this->posts
                 ->orderBy('created_at', 'desc')
                 ->where('user_id', $id)
                 ->with('category')
                 ->with('user')
                 ->get();
-  	}
+    }
 
-  	public function create($inputs)
-  	{
-    	return $this->posts->create($inputs); 			
-  	}
+    public function create($inputs)
+    {
+        return $this->posts->create($inputs);           
+    }
 
-  	public function getById($id)
-  	{
-  		return $this->posts
+    public function getById($id)
+    {
+        return $this->posts
                 ->where('id', $id)
                 ->with('category')
                 ->with('user')
                 ->first();
-  	}
+    }
 
-  	public function update($inputs, $id)
-  	{
-  		$post = $this->posts->find($id);
+    public function update($inputs, $id)
+    {
+        $post = $this->posts->find($id);
         $default_image = 'empty.jpg';   
         $image_path = public_path('/images').'/'.$post->image_path;
         $image = $inputs['image_path'];
         if ($post->image_path != $default_image && !is_null($image)) {
             File::delete($image_path);
         };
-  		return $this->posts
-  			 	->where('id', $id)
-  			 	->with('category')
-  			 	->with('user')
-  			 	->update($inputs);
-  	}
-  	public function delete($id)
-  	{
-  		$post = $this->posts->find($id);
-  		$default_image = 'empty.jpg';
+        return $this->posts
+                ->where('id', $id)
+                ->with('category')
+                ->with('user')
+                ->update($inputs);
+    }
+    public function delete($id)
+    {
+        $post = $this->posts->find($id);
+        $default_image = 'empty.jpg';
         $image_path = public_path('/images').'/'.$post->image_path;
         if ($post->image_path != $default_image) {
             File::delete($image_path);
         };
-  		$this->posts->where('id', $id)->delete();
-  	}
+        $this->posts->where('id', $id)->delete();
+    }
 }
